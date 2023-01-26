fb38b8f28ffbf97e7a4d52099d60af5ae514b336
Aleks <45088@mail.ru>
2023-01-27 00:49:45 +0300

    v1

---
+++ script.js
@@ -0,0 +1,89 @@
+const p1=document.querySelector('.p1')
+const p2=document.querySelector('.p2')
+const p3=document.querySelector('.p3')
+const p4=document.querySelector('.p4')
+const p5=document.querySelector('.p5')
+const p6=document.querySelector('.p6')
+const p7=document.querySelector('.p7')
+const p8=document.querySelector('.p8')
+const p9=document.querySelector('.p9')
+const p10=document.querySelector('.p10')
+const p11=document.querySelector('.p11')
+const p12=document.querySelector('.p12')
+const p13=document.querySelector('.p13')
+const p14=document.querySelector('.p14')
+const p15=document.querySelector('.p15')
+const p16=document.querySelector('.p16')
+const p17=document.querySelector('.p17')
+const p18=document.querySelector('.p18')
+const p19=document.querySelector('.p19')
+const p20=document.querySelector('.p20')
+const p21=document.querySelector('.p21')
+
+p1.addEventListener('click',(()=>{
+    open('../docs/Basi_Standard.pdf')
+}))
+p2.addEventListener('click',(()=>{
+    open('../docs/Core_Standard.pdf')
+    console.log('1')
+}))
+p3.addEventListener('click',(()=>{
+    open('../docs/Базовый стандарт совершения МФО операций на финансовом рынке.pdf')
+    console.log('1')
+}))
+p4.addEventListener('click',(()=>{
+    open('../docs/Блок-схема аффилированных лиц (1).pdf')
+}))
+p5.addEventListener('click',(()=>{
+    open('../docs/Выписка из протокола СРО.pdf')
+}))
+p6.addEventListener('click',(()=>{
+    open('../docs/Выписка МФО.pdf')
+}))
+p7.addEventListener('click',(()=>{
+    open('../docs/Закон о защите прав потребителей.pdf')
+}))
+p8.addEventListener('click',(()=>{
+    open('../docs/Закон о микрофинансовой деятельности и микрофинансовых организациях.pdf')
+    console.log('8')
+}))
+p9.addEventListener('click',(()=>{
+    open('../docs/Закон о потребительском кредите (займе).pdf')
+}))
+p10.addEventListener('click',(()=>{
+    open('../docs/Информация об условиях выдачи микрозаймов.pdf')
+}))
+p11.addEventListener('click',(()=>{
+    open('../docs/Лист записи ЕГРЮЛ (ОГРН).pdf')
+}))
+p12.addEventListener('click',(()=>{
+    open('../docs/Образец индивидуальных условий договора.pdf')
+}))
+p13.addEventListener('click',(()=>{
+    open('../docs/Общие условия договора микрозайма.pdf')
+}))
+p14.addEventListener('click',(()=>{
+    open('../docs/Памятка заемщику - МФК и МКК.pdf')
+}))
+p15.addEventListener('click',(()=>{
+    open('../docs/Памятка заемщику - понятие МФО.pdf')
+    console.log('15')
+}))
+p16.addEventListener('click',(()=>{
+    open('../docs/Политика защиты персональных данных.pdf')
+}))
+p17.addEventListener('click',(()=>{
+    open('../docs/Правила предоставления микрозаймов.pdf')
+}))
+p18.addEventListener('click',(()=>{
+    open('../docs/Свидетельство ИНН.pdf')
+}))
+p19.addEventListener('click',(()=>{
+    open('../docs/Список аффилированных лиц и оказывающих существенное влияние (1).pdf')
+}))
+p20.addEventListener('click',(()=>{
+    open('../docs/Список ОСП по состоянию на 10.12.2021.pdf')
+}))
+p21.addEventListener('click',(()=>{
+    open('../docs/Устав ООО МКК Центр Денег 2 ред.pdf')
+}))
